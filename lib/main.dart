aaa70793c97d3dd27066fe92e1df27fc8b099aee
AmonKogei <amonlatimo101@gmail.com>
2023-10-27 13:51:57 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,274 @@
+// ignore_for_file: depend_on_referenced_packages, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double baseWidth = 380;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          // spashscreenpXm (1:2863)
+          width: double.infinity,
+          height: 812 * fem,
+          decoration: BoxDecoration(
+            color: Color(0xfff7f7f3),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                // image3i7M (1:2865)
+                left: 0 * fem,
+                top: 0 * fem,
+                child: Align(
+                  child: SizedBox(
+                      width: 375 * fem,
+                      height: 811 * fem,
+                      child: Image.network(
+                        'https://files.sophie.co.ke/2022/11/0-main-plaid-pu-leather-women-shoulder-bag-chain-strap-crossbody-bags-for-women-fashion-designer-handbags-female-messenger-bags_4908.png',
+                        fit: BoxFit.cover,
+                      )),
+                ),
+              ),
+              Positioned(
+                // rectangle15bwq (1:2866)
+                left: 0 * fem,
+                top: 280 * fem,
+                child: Align(
+                  child: SizedBox(
+                    width: 375 * fem,
+                    height: 531 * fem,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment(0, -1),
+                          end: Alignment(0, 1),
+                          colors: <Color>[Color(0x00000000), Color(0xff000000)],
+                          stops: <double>[0, 1],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                // g3141sPZ (1:2867)
+                left: 68 * fem,
+                top: 537 * fem,
+                child: Align(
+                  child: SizedBox(
+                      width: 22 * fem,
+                      height: 28 * fem,
+                      child: Image.network(
+                        'https://i.pinimg.com/originals/15/85/0b/15850b7eb0a5f4a805c059f8f91be3e9.jpg',
+                        width: 22 * fem,
+                        height: 28 * fem,
+                      )),
+                ),
+              ),
+              Positioned(
+                // path3034AdZ (1:2879)
+                left: 76.1307373047 * fem,
+                top: 552.4304199219 * fem,
+                child: Align(
+                  child: SizedBox(
+                    width: 0.92 * fem,
+                    height: 1.51 * fem,
+                    child: Image.network(
+                      'https://i.pinimg.com/originals/15/85/0b/15850b7eb0a5f4a805c059f8f91be3e9.jpg',
+                      width: 0.92 * fem,
+                      height: 1.51 * fem,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                // bagmoresXy (1:2880)
+                left: 95 * fem,
+                top: 528 * fem,
+                child: Align(
+                  child: SizedBox(
+                    width: 176 * fem,
+                    height: 55 * fem,
+                    child: Text(
+                      'Bag More',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.abel(
+                        fontSize: 40 * ffem,
+                        fontWeight: FontWeight.w400,
+                        height: 1.355 * ffem / fem,
+                        letterSpacing: -1.5 * fem,
+                        color: Color(0xffffffff),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                // rectangle148yh (1:2881)
+                left: 99 * fem,
+                top: 651 * fem,
+                child: Align(
+                  child: SizedBox(
+                    width: 177 * fem,
+                    height: 52 * fem,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xffffffff),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0x3f000000),
+                            offset: Offset(0 * fem, 5 * fem),
+                            blurRadius: 5 * fem,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                // letsstartQwD (1:2882)
+                left: 146 * fem,
+                top: 663 * fem,
+                child: Align(
+                  child: SizedBox(
+                    width: 83 * fem,
+                    height: 28 * fem,
+                    child: Text(
+                      'Let’s start',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.abel(
+                        fontSize: 20 * ffem,
+                        fontWeight: FontWeight.w400,
+                        height: 1.355 * ffem / fem,
+                        letterSpacing: -1 * fem,
+                        color: Color(0xff000000),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                // addingmomentsofstylesHVD (1:2883)
+                left: 86.5 * fem,
+                top: 589 * fem,
+                child: Align(
+                  child: SizedBox(
+                    width: 220 * fem,
+                    height: 24 * fem,
+                    child: Text(
+                      'Adding Moments of Styles',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.dmSans(
+                        fontSize: 18 * ffem,
+                        fontWeight: FontWeight.w400,
+                        height: 1.3025 * ffem / fem,
+                        color: Color(0xffffffff),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                // rectangle16MV5 (1:2884)
+                left: 112 * fem,
+                top: 771 * fem,
+                child: Align(
+                  child: SizedBox(
+                    width: 144 * fem,
+                    height: 3 * fem,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(200 * fem),
+                        border: Border.all(color: Color(0xffffffff)),
+                        color: Color(0xffffffff),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                // barsstatusbariphonelighteyy (1:2885)
+                left: 30.5 * fem,
+                top: 16.3332519531 * fem,
+                child: Container(
+                  width: 326.16 * fem,
+                  height: 19 * fem,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        // timekGK (I1:2885;0:4895;0:4961)
+                        margin: EdgeInsets.fromLTRB(
+                            0 * fem, 0 * fem, 232.5 * fem, 0 * fem),
+                        child: Text(
+                          '9:41',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.dmSans(
+                            fontSize: 15 * ffem,
+                            fontWeight: FontWeight.w600,
+                            height: 1.2575 * ffem / fem,
+                            letterSpacing: -0.3000000119 * fem,
+                            color: Color(0xff000000),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        // cellularconnection427 (I1:2885;0:4892)
+                        margin: EdgeInsets.fromLTRB(
+                            0 * fem, 0 * fem, 5 * fem, 1.67 * fem),
+                        width: 17 * fem,
+                        height: 10.67 * fem,
+                        child: Image.network(
+                          'https://www.pngegg.com/en/png-trvdj',
+                          width: 17 * fem,
+                          height: 10.67 * fem,
+                        ),
+                      ),
+                      Container(
+                        // wifikfd (I1:2885;0:4888)
+                        margin: EdgeInsets.fromLTRB(
+                            0 * fem, 0 * fem, 5 * fem, 2.01 * fem),
+                        width: 15.33 * fem,
+                        height: 11 * fem,
+                        child: Image.network(
+                          'https://www.pngegg.com/en/png-ifzvk',
+                          width: 15.33 * fem,
+                          height: 11 * fem,
+                        ),
+                      ),
+                      Container(
+                        // batteryV7R (I1:2885;0:4882)
+                        margin: EdgeInsets.fromLTRB(
+                            0 * fem, 0 * fem, 0 * fem, 1.67 * fem),
+                        width: 24.33 * fem,
+                        height: 11.33 * fem,
+                        child: Image.network(
+                          'https://www.pngegg.com/en/png-ktbpp',
+                          width: 24.33 * fem,
+                          height: 11.33 * fem,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
